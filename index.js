a6986c7e49474d5ed740362cdde879df654efb36
Nick Teinert <nteinert2005@gmail.com>
2023-03-09 12:24:31 -0600

    update package

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 const CartSync = require("./src/cartsync");
 
-const cartsync = new CartSync({
-  cartsync: "https://revivalanimal-ih2.cartsync.com",
-  bigcommerce: "https://api.bigcommerce.com/stores/3xvwarb9yi",
-  storeApiToken: "hp1e35bm8d3lkjx11d2mkj5tpviix9x",
-  storeApiClientId: "66u93cfded0c6ilqbsy0ebti1cmwroi",
-});
-
-cartsync.getBrand("zoetis");
-
-// module.exports = {
-//   CartSync: CartSync,
-// };
+module.exports = {
+  CartSync: CartSync,
+};
